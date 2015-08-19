5dce75481e09f5d20cc2944425259a4da7238bb4
matthewcosgrove <mcosgrove@pivotal.io>
2015-08-19 15:24:22 +0100

    Initial commit of Spring Boot app with basic set up of security and actuator

---
+++ src/main/java/com/emc/demo/ActuatorCucumberDemoApplication.java
@@ -0,0 +1,12 @@
+package com.emc.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActuatorCucumberDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ActuatorCucumberDemoApplication.class, args);
+    }
+}
